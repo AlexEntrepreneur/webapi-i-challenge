e1960c7ab52da28353a1653f5de9ed9b50a4adcb
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-18 14:45:43 +0000

    Implement GET users endpoint

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // implement your API here
 const express = require('express');
-
 const server = express();
-
-server.listen(3300, () =>
-  console.log('Server listening on port 3300')
-);
+const db = require('./data/db.js');
 
 server.get('/', (req, res) => {
   res.send('Nothing to see here... try /api/');
 });
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(data => res.json(data))
+    .catch(data => res.status('500').json({
+      "error": "The users information could not be retrieved."
+    }));
+});
+
+server.listen(3300, () =>
+  console.log('Server listening on port 3300')
+);
