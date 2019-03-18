c9a4c0927c8884cda6a7980d07829b34489da7b1
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-18 14:26:40 +0000

    Initial Setup Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.listen(3300, () =>
+  console.log('Server listening on port 3300')
+);
+
+server.get('/', (req, res) => {
+  res.send('Nothing to see here... try /api/');
+});
