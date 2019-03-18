c4d0068e226a9727f06ef74af0234cc9b6ba9cae
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-18 15:48:19 +0000

    Implement Add User Endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // implement your API here
 const express = require('express');
 const server = express();
+server.use(express.json());
 const db = require('./data/db.js');
 
 server.get('/', (req, res) => {
@@ -10,7 +11,7 @@ server.get('/', (req, res) => {
 server.get('/api/users', (req, res) => {
   db.find()
     .then(data => res.json(data))
-    .catch(data => res.status('500').json({
+    .catch(data => res.status(500).json({
       "error": "The users information could not be retrieved."
     }));
 });
@@ -23,17 +24,35 @@ server.get('/api/users/:id', (req, res) => {
       res.json(data)
     }
     else {
-      res.status('404').json({
+      res.status(404).json({
         "message": `The user with the specified ID \"${id}\" does not exist.`
       })
     }
   })
   .catch(err => {
-    res.status('500').json({
+    res.status(500).json({
       error: "The user information could not be retrieved."
     });
   });
 });
+
+server.post('/api/users', (req, res) => {
+  const validBodyProvided = req.body.name && req.body.bio;
+  if (validBodyProvided) {
+    db.insert(req.body)
+      .then(data => {
+        res.status(201).json(data);
+      })
+      .catch(error => res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      }))
+  }
+  else {
+    res.status(404).json({
+      errorMessage: "Please provide name and bio for the user."
+    });
+  }
+})
 
 server.listen(3300, () =>
   console.log('Server listening on port 3300')
